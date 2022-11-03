c819e3f295f80130fbe11d7250bf6befbabb4c54
ganesh314159 <ganesh314159@gmail.com>
2022-11-03 20:57:48 +0530

    website: move templates and static to root directory
    Signed-off-by: ganesh314159 <ganesh314159@gmail.com>

--- mathletics/settings.py
+++ mathletics/settings.py
@@ -59,8 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            "home/templates",
-            "blog/templates"
+            "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,9 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
